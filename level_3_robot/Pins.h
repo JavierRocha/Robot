942dc5e00f71322bb1e28919d41cf6b2184b2258
JavierRocha <jav11@live.com>
2017-11-02 13:35:38 -0500

    Add files via upload

---
+++ level_3_robot/Pins.h
@@ -0,0 +1,24 @@
+#ifndef _PINS_H_
+#define _PINS_H_
+
+#define MALLET_PIN        10
+
+#define MOTOR_LEFT_CH0    9
+#define MOTOR_LEFT_CH1    8
+#define MOTOR_RIGHT_CH0   12
+#define MOTOR_RIGHT_CH1   11
+//#define MOTOR_RIGHT_CH0   10
+//#define MOTOR_RIGHT_CH1   9
+
+#define PING_FRONT_TRIG   2
+#define PING_FRONT_ECHO   3
+#define PING_LEFT_TRIG    A0
+#define PING_LEFT_ECHO    A1
+#define PING_RIGHT_TRIG   4
+#define PING_RIGHT_ECHO   2
+
+#define XBEE_DIN   14
+#define XBEE_DOUT   15
+
+#endif
+
